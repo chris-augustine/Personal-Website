ba46abc9a45b22c94a8d1e20bd3394dcd242d683
Chris Augustine <caugustine4@gmail.com>
2025-01-10 21:27:34 -0800

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,11 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.linkedin.com/in/chris--augustine/" target="_blank">
+          <img src="/Profile_Picture_Yearbook.jpg" className="logo" alt="Photo of Chris Augustine" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chris Augustine Personal Website</h1>
     </div>
   )
 }
